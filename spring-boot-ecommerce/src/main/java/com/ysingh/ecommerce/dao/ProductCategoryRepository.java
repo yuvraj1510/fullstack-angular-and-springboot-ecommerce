24e344f5c63eb2377981b4eb7e9ba9db53632902
yuvrajs <yuvraj1510@gmail.com>
2022-09-21 01:46:27 +0530

    Adding Angular E-Commerce folder

--- spring-boot-ecommerce/src/main/java/com/ysingh/ecommerce/dao/ProductCategoryRepository.java
+++ spring-boot-ecommerce/src/main/java/com/ysingh/ecommerce/dao/ProductCategoryRepository.java
@@ -2,9 +2,11 @@ package com.ysingh.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ysingh.ecommerce.entity.ProductCategory;
 
+@CrossOrigin
 @RepositoryRestResource(collectionResourceRel = "productCategory", path = "product-category")
 public interface ProductCategoryRepository extends JpaRepository<ProductCategory, Long> {
 
--- spring-boot-ecommerce/src/main/java/com/ysingh/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/ysingh/ecommerce/dao/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.ysingh.ecommerce.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ysingh.ecommerce.entity.Product;
 
+@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
 }
