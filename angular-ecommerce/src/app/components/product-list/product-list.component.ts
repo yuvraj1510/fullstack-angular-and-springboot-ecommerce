3e32b4bb34c8e1ee06ef09dbf3ad9e5c8eb69682
yuvrajs <yuvraj1510@gmail.com>
2022-09-26 22:18:36 +0530

    Adding Master view and Pagination Support

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -12,7 +12,14 @@ export class ProductListComponent implements OnInit {
 
   products: Product[] = [];
   currentCategoryId: number = 1;
+  previousCategoryId: number = 1;
   searchMode: boolean = false;
+
+  thePageNumber: number = 1;
+  thePageSize: number = 5;
+  theTotalElements: number = 0;
+
+  previousKeyword!: string;
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -42,21 +49,46 @@ export class ProductListComponent implements OnInit {
       this.currentCategoryId = 1;
     }
 
-    this.productService.getProductList(this.currentCategoryId).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    if(this.previousCategoryId != this.currentCategoryId) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousCategoryId = this.currentCategoryId;
+
+    console.log(`currentCategoryId=${this.currentCategoryId}, thePageNumber=${this.thePageNumber}`)
+
+    this.productService.getProductListPaginate(this.thePageNumber-1, 
+                                               this.thePageSize, 
+                                               this.currentCategoryId).subscribe(this.processResult());
   }
 
   handleSeachProducts() {
+
     const theKeyWord: string = this.route.snapshot.paramMap.get('keyword')!;
 
-    this.productService.searchProducts(theKeyWord).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    if(this.previousKeyword != theKeyWord) {
+      this.thePageNumber = 1;
+    }
 
+    this.previousKeyword = theKeyWord;
+
+    this.productService.searchProductListPaginate(this.thePageNumber-1, 
+                                               this.thePageSize, 
+                                               theKeyWord).subscribe(this.processResult());
+  }
+
+  updatePageSize(pageSize: string) {
+    this.thePageSize = +pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
+  }
+
+  processResult() {
+    return (data:any) => {
+      this.products = data._embedded.products;
+      this.thePageNumber = data.page.number + 1;
+      this.thePageSize = data.page.size;
+      this.theTotalElements = data.page.totalElements;
+    }
   }
 }
--- angular-ecommerce/src/app/services/product.service.ts
+++ angular-ecommerce/src/app/services/product.service.ts
@@ -22,10 +22,26 @@ export class ProductService {
     return this.getProducts(searchUrl);
   }
 
+  getProductListPaginate(thePage: number, 
+                         thePageSize: number,
+                         theCategoryId: number): Observable<GetResponseProducts> {
+
+    const searchUrl = `${this.baseUrl}/search/findByCategoryId?id=${theCategoryId}&page=${thePage}&size=${thePageSize}`;
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
+  }
+
   searchProducts(theKeyWord: string): Observable<Product[]> {
 
     const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyWord}`
     return this.getProducts(searchUrl);
+  }
+
+  searchProductListPaginate(thePage: number, 
+                            thePageSize: number,
+                            theKeyWord: string): Observable<GetResponseProducts> {
+
+    const searchUrl = `${this.baseUrl}/search/findByNameContaining?name=${theKeyWord}&page=${thePage}&size=${thePageSize}`;
+    return this.httpClient.get<GetResponseProducts>(searchUrl);
   }
 
   getProduct(theProductId: number): Observable<Product> {
@@ -50,6 +66,12 @@ export class ProductService {
 interface GetResponseProducts {
   _embedded: {
     products: Product[];
+  },
+  page: {
+    size: number,
+    totalElements: number,
+    totalPages: number,
+    number: number
   }
 }
 
