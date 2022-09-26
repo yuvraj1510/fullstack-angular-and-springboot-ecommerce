d73c2f5e8f9ce0fb9bfbf31aa1511967166cda1c
yuvrajs <yuvraj1510@gmail.com>
2022-09-26 18:05:16 +0530

    Adding Search Capability

--- spring-boot-ecommerce/src/main/java/com/ysingh/ecommerce/dao/ProductRepository.java
+++ spring-boot-ecommerce/src/main/java/com/ysingh/ecommerce/dao/ProductRepository.java
@@ -1,11 +1,16 @@
 package com.ysingh.ecommerce.dao;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.ysingh.ecommerce.entity.Product;
 
 @CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	
+	Page<Product> findByCategoryId(@Param("id") Long id, Pageable pageable);
 
 }
