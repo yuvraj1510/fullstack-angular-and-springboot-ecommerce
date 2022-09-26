cfdce35540024e12e90445b0439568f4532f5ef9
yuvrajs <yuvraj1510@gmail.com>
2022-09-27 00:34:42 +0530

    Add Cart Status Implementation

--- angular-ecommerce/src/app/components/product-list/product-list.component.ts
+++ angular-ecommerce/src/app/components/product-list/product-list.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { CartItem } from 'src/app/common/cart-item';
 import { Product } from 'src/app/common/product';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductService } from 'src/app/services/product.service';
 
 @Component({
@@ -22,6 +24,7 @@ export class ProductListComponent implements OnInit {
   previousKeyword!: string;
 
   constructor(private productService: ProductService,
+              private cartService: CartService,
               private route: ActivatedRoute) { }
 
   ngOnInit(): void {
@@ -90,5 +93,10 @@ export class ProductListComponent implements OnInit {
       this.thePageSize = data.page.size;
       this.theTotalElements = data.page.totalElements;
     }
+  }
+
+  addToCart(theProduct: Product) {
+     const theCartItem = new CartItem(theProduct);
+     this.cartService.addToCart(theCartItem);
   }
 }
